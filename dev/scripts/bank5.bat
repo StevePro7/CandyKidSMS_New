4c0e9eded5c4df36e7770f665f5f544888c761d9
StevePro7 <steveprozune@hotmail.com>
2020-02-07 12:37:30 +0000

    no message

--- dev/scripts/bank5.bat
+++ dev/scripts/bank5.bat
@@ -3,16 +3,9 @@ REM bank5
 
 cd ..
 cd banks
-cd bank5
 
-:: Tiles
-bmp2tile.exe raw\main_tiles.png -savetiles "main_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 64 -savetilemap "main_tiles (tilemap).bin" -savepalette "main_tiles (palette).bin" -fullpalette -exit
-
-:: Tiles
-bmp2tile.exe raw\splash.bmp -savetiles "splash (tiles).psgcompr" -removedupes -nomirror -planar -tileoffset 128 -savetilemap "splash (tilemap).stmcompr" -savepalette "splash (palette).bin" -fullpalette -exit
-
-cd ..
 folder2c bank5 bank5 5
+
 sdcc -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK5 bank5.c
 
 del *.asm > nul
