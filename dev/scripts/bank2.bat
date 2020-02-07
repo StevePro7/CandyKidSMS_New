4690c64e3ae482b9f1d272d1cfe9ab9e661b00d6
StevePro7 <steveprozune@hotmail.com>
2020-02-07 13:16:45 +0000

    no message

--- dev/scripts/bank2.bat
+++ dev/scripts/bank2.bat
@@ -3,10 +3,13 @@ REM echo Generate levels
 
 cd ..
 cd banks
+cd bank2
 
 :: Tiles
 bmp2tile.exe raw\font_tiles.bmp -savetiles "font_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 0  -savetilemap "font_tiles (tilemap).bin" -savepalette "font_tiles (palette).bin" -fullpalette -exit
 bmp2tile.exe raw\game_tiles.png -savetiles "game_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 64 -savetilemap "game_tiles (tilemap).bin" -savepalette "game_tiles (palette).bin" -fullpalette -exit
+bmp2tile.exe raw\main_tiles.png -savetiles "main_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 64 -savetilemap "main_tiles (tilemap).bin" -savepalette "main_tiles (palette).bin" -fullpalette -exit
+bmp2tile.exe raw\splash.bmp -savetiles "splash (tiles).psgcompr" -removedupes -nomirror -planar -tileoffset 128 -savetilemap "splash (tilemap).stmcompr" -savepalette "splash (palette).bin" -fullpalette -exit
 
 :: Sprites
 :: https://github.com/maxim-zhao/bmp2tile
@@ -14,7 +17,6 @@ bmp2tile.exe raw\sprites.png -savetiles "sprites (tiles).psgcompr" -noremovedupe
 
 
 cd ..
-
 folder2c bank2 bank2 2
 sdcc -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK2 bank2.c
 
