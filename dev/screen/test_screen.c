6144ab21ae785957b4d18afb23cfea896bc900c8
StevePro7 <steveprozune@hotmail.com>
2020-02-08 14:44:00 +0000

    no message

--- dev/screen/test_screen.c
+++ dev/screen/test_screen.c
@@ -1,13 +1,39 @@
 #include "test_screen.h"
 #include "..\engine\enum_manager.h"
 #include "..\engine\font_manager.h"
+#include "..\engine\input_manager.h"
+#include "..\banks\bank15.h"
+#include "..\banks\fixedbank.h"
+#include "..\devkit\_sms_manager.h"
 
 void screen_test_screen_load()
 {
+	unsigned char bob;
+	//bob = foo[ 0 ];
+	devkit_SMS_mapROMBank( 15 );
+	bob = bank15_txt[ 0 ];
+	//bob = 7;
+	engine_font_manager_draw_data( bob, 10, 4 );
+	//engine_font_manager_draw_char( bob, 10, 5 );
 	engine_font_manager_draw_text( "TEST SCREEN!!", 10, 10 );
 }
 
 void screen_test_screen_update( unsigned char *screen_type )
 {
+	unsigned char test;
+	//unsigned char data;
+
+	//devkit_SMS_mapROMBank( 15 );
+	//data = bank15_txt[ 0 ];
+
+	test = engine_input_manager_hold_fire1();
+	if( test )
+	{
+		engine_font_manager_draw_data( bank15_txt[ 0 ], 10, 14 );
+		//bank15_txt[ 0 ] = 66;
+		//data = bank15_txt[ 0 ];
+		//engine_font_manager_draw_text( "FIRE", 10, 12 );
+		engine_font_manager_draw_data( bank15_txt[ 0 ], 10, 15 );
+	}
 	*screen_type = screen_type_test;
 }
