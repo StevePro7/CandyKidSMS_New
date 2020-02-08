0ed1cdddf1c83759a15064b2aac5efaaaa3ea2ef
StevePro7 <steveprozune@hotmail.com>
2020-02-08 19:29:54 +0000

    no message

--- dev/scripts/bank4.bat
+++ dev/scripts/bank4.bat
@@ -8,9 +8,7 @@ folder2c bank4 bank4 4
 
 sdcc -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK4 bank4.c
 
-del *.asm > nul
-del *.lst > nul
-del *.sym > nul
+del *.asm > nul; del *.lst > nul; del *.sym > nul
 
 cd ..
 cd scripts
--- dev/scripts/bank5.bat
+++ dev/scripts/bank5.bat
@@ -8,9 +8,7 @@ folder2c bank5 bank5 5
 
 sdcc -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK5 bank5.c
 
-del *.asm > nul
-del *.lst > nul
-del *.sym > nul
+del *.asm > nul; del *.lst > nul; del *.sym > nul
 
 cd ..
 cd scripts
