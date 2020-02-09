7faee1776292ec4d0938dc941f104e512291d6fd
StevePro7 <steveprozune@hotmail.com>
2020-02-09 17:31:54 +0000

    no message

--- dev/banks/fixedbank.h
+++ dev/banks/fixedbank.h
@@ -25,3 +25,7 @@ extern const unsigned char board_object_right;
 extern const unsigned char *level_object_AAdata[];
 extern const unsigned char level_object_AAsize[];
 extern const unsigned char level_object_AAbank[];
+
+//extern const unsigned char *level_object_BBdata[];
+//extern const unsigned char level_object_BBsize[];
+//extern const unsigned char level_object_BBbank[];
