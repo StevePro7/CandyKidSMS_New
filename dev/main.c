2622012d1301b6fbebaa731e89a9ce1f12f82ac2
StevePro7 <steveprozune@hotmail.com>
2020-02-27 10:26:16 +0000

    no message

--- dev/main.c
+++ dev/main.c
@@ -30,7 +30,7 @@ void main (void)
 	//open_screen_type = screen_type_dead;
 	//open_screen_type = screen_type_over;
 	//open_screen_type = screen_type_demo;
-	open_screen_type = screen_type_pass;
+	open_screen_type = screen_type_test;
 	//open_screen_type = screen_type_reset;
 	//open_screen_type = screen_type_play;
 	//open_screen_type = screen_type_intro;
