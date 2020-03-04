bb37509642ad43fb50dc8cb5c132fa05ed4d7505
StevePro7 <steveprozune@hotmail.com>
2020-03-04 22:28:08 +0000

    no message

--- dev/screen/func_screen.c
+++ dev/screen/func_screen.c
@@ -12,46 +12,46 @@ static unsigned char first_time;
 
 void screen_func_screen_load()
 {
-	engine_score_manager_load();
-	engine_score_manager_draw_text();
-	engine_score_manager_draw_all();
+	//engine_score_manager_load();
+	//engine_score_manager_draw_text();
+	//engine_score_manager_draw_all();
 
-	engine_font_manager_draw_text( "FUNC SCREEN", 10, 2 );
+	//engine_font_manager_draw_text( "FUNC SCREEN", 10, 2 );
 }
 
 void screen_func_screen_update( unsigned char *screen_type )
 {
-	unsigned char input;
+	//unsigned char input;
 
-	input = engine_input_manager_hold( input_type_up );
-	if( input )
-	{
-		engine_score_manager_update_oneup();
-	}
-	input = engine_input_manager_hold( input_type_down );
-	if( input )
-	{
-		engine_score_manager_update_lives( -1 );
-	}
-	input = engine_input_manager_hold( input_type_left );
-	if( input )
-	{
-		engine_score_manager_update_candy();
-	}
-	input = engine_input_manager_hold( input_type_right );
-	if( input )
-	{
-		engine_score_manager_update_bonus( tile_type_bonusD );
-	}
+	//input = engine_input_manager_hold( input_type_up );
+	//if( input )
+	//{
+	//	engine_score_manager_update_oneup();
+	//}
+	//input = engine_input_manager_hold( input_type_down );
+	//if( input )
+	//{
+	//	engine_score_manager_update_lives( -1 );
+	//}
+	//input = engine_input_manager_hold( input_type_left );
+	//if( input )
+	//{
+	//	engine_score_manager_update_candy();
+	//}
+	//input = engine_input_manager_hold( input_type_right );
+	//if( input )
+	//{
+	//	engine_score_manager_update_bonus( tile_type_bonusD );
+	//}
 
-	//input = engine_input_manager_hold( input_type_fire1 );
-	input = engine_input_manager_move( input_type_fire1 );
-	if( input )
-	{
-		engine_score_manager_update_boost();
-		//engine_score_manager_update_candy();
-	}
+	////input = engine_input_manager_hold( input_type_fire1 );
+	//input = engine_input_manager_move( input_type_fire1 );
+	//if( input )
+	//{
+	//	engine_score_manager_update_boost();
+	//	//engine_score_manager_update_candy();
+	//}
 
-	//first_time = 0;
+	////first_time = 0;
 	*screen_type = screen_type_func;
 }
