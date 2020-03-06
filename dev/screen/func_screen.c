c378d25f80c0e0cb667852499ca9d5324ed72982
StevePro7 <steveprozune@hotmail.com>
2020-03-06 13:25:13 +0000

    no message

--- dev/screen/func_screen.c
+++ dev/screen/func_screen.c
@@ -1,23 +1,52 @@
 #include "func_screen.h"
 #include "..\engine\board_manager.h"
+#include "..\engine\enemy_manager.h"
 #include "..\engine\enum_manager.h"
 #include "..\engine\font_manager.h"
 #include "..\engine\input_manager.h"
 #include "..\engine\level_manager.h"
 #include "..\engine\memo_manager.h"
+#include "..\engine\sprite_manager.h"
+#include "..\engine\tile_manager.h"
 #include "..\devkit\_sms_manager.h"
 #include "..\banks\databank.h"
 #include "..\banks\fixedbank.h"
 
-static unsigned char first_time;
+static void test_fixed();
 
 void screen_func_screen_load()
 {
-	//unsigned char diff = diff_type_easy;
-	////unsigned char diff = diff_type_hard;
-	////unsigned char pace = pace_type_slow;
-	//unsigned char pace = pace_type_fast;
+	engine_board_manager_draw_full();
+	engine_memo_manager_draw( 26, 27 );
+	//engine_font_manager_draw_text( "FUNC SCREEN...!", 10, 2 );
+}
 
+// TODO - in order for this to work I must have an overload for draw trees
+// 	draw_tile_priority( offset, x, y );
+void screen_func_screen_update( unsigned char *screen_type )
+{
+	//unsigned char x = 64;
+	//unsigned char y = 56;
+	// minX = 60
+	// minY = 60
+	// maxX = 148
+	// maxY = 116
+	unsigned char x = 128;
+	unsigned char y = 116;
+	//devkit_SMS_addSprite( x, y + 8, 256 );
+	engine_sprite_manager_draw_entity( x, y, 256 + 96 );
+	engine_sprite_manager_draw_entity( x, 60, 256 + 96 );
+
+	engine_sprite_manager_draw_entity( 60, 100, 256 + 96 );
+	engine_sprite_manager_draw_entity( 148, 100, 256 + 96 );
+	
+	*screen_type = screen_type_func;
+}
+
+
+
+static void test_fixed()
+{
 	unsigned char enemy = actor_type_suz;
 	unsigned char index;
 	unsigned char speed;
@@ -35,9 +64,4 @@ void screen_func_screen_load()
 	engine_font_manager_draw_text( "FUNC SCREEN!", 10, 2 );
 
 	engine_memo_manager_option();
-}
-
-void screen_func_screen_update( unsigned char *screen_type )
-{
-	*screen_type = screen_type_func;
 }
