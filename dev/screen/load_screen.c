1403e1d0ab4344cafb4f812bad48d6e3635ca9b6
StevePro7 <steveprozune@hotmail.com>
2020-03-08 15:49:47 +0000

    no message

--- dev/screen/load_screen.c
+++ dev/screen/load_screen.c
@@ -17,8 +17,8 @@
 #include "..\banks\databank.h"
 #include "..\banks\fixedbank.h"
 
-//#define LOAD_SCREEN_DELAY	150
-#define LOAD_SCREEN_DELAY	15
+#define LOAD_SCREEN_DELAY	150
+//#define LOAD_SCREEN_DELAY	15
 //#define LOAD_SCREEN_DELAY	1
 
 static void print_level();
@@ -66,7 +66,7 @@ void screen_load_screen_update( unsigned char *screen_type )
 	input = engine_input_manager_hold( input_type_fire1 );
 	if( delay || input )
 	{
-		engine_level_manager_draw_middle();
+		//engine_level_manager_draw_middle();
 		engine_audio_manager_music_play( music_type_game03 );
 		*screen_type = state_object_next_screen;
 		return;
