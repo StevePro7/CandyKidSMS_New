1937ff0d42e7b42a26ae47496657b421c558888f
StevePro7 <steveprozune@hotmail.com>
2020-03-20 13:38:39 +0000

    devkit ROM bank

--- dev/engine/locale_manager.c
+++ dev/engine/locale_manager.c
@@ -6,7 +6,8 @@
 
 void engine_locale_manager_draw_text( unsigned char index, unsigned char x, unsigned char y )
 {
-	const unsigned char *text = locale_object_texts[ index ];
+	const unsigned char *text;
 	devkit_SMS_mapROMBank( FIXED_BANK );
+	text = locale_object_texts[ index ];
 	engine_font_manager_draw_text( text, x, y );
 }
